264cf096b1dae93b9d67e0748bf847011d75310b
jpcerrone <jpcerronex@gmail.com>
2023-09-22 14:39:48 -0300

    touch screen buttons

--- source/main.c
+++ source/main.c
@@ -60,8 +60,8 @@ int main(void) {
 	videoSetModeSub(MODE_0_2D);
 	vramSetBankA(VRAM_A_MAIN_BG);
 	vramSetBankD(VRAM_D_SUB_SPRITE);
-
-	//consoleDemoInit();
+	
+	consoleDemoInit();
 	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0 /*the 2k offset into vram the tile map will be placed*/,0 /* the 16k offset into vram the tile graphics data will be placed*/);
 	dmaCopy(floor_bBitmap, bgGetGfxPtr(bg3), floor_bBitmapLen);
 	dmaCopy(floor_bPal, BG_PALETTE, floor_bPalLen); 
@@ -72,13 +72,33 @@ int main(void) {
 	memset(sprites, 0, sizeof(Sprite)*MAX_SPRITES);
 
 	// Display buttons
+	Sprite* buttons[10];
+	memset(buttons, 0, sizeof(buttons));
 	for(int y=0;y < 10; y++){
 		int x = SCREEN_SIZE.width/2 - 16;
 		x += (y%2 == 0) ? 14 : -14;
-		createSprite(x, y * 16 + 8, 32, 32, 0, (uint8_t*)buttonBigTiles);
+		buttons[y] = createSprite(x, y * 16 + 8, 32, 32, 0, (uint8_t*)buttonBigTiles);
 	}
 	//float delta = 0.01666666666;
+	touchPosition touch;
+	bool lastFramePenDown = false;
 	while(1) {
+		// Touch controls
+		touchRead(&touch);
+		bool penDown = (touch.px != 0) || (touch.py != 0);
+		if (penDown && !lastFramePenDown){
+			for(int i=0; i < 10; i++){ // TODO: This could be done with a radius for more circular precision
+				if ((touch.px >= buttons[i]->x) && (touch.px <= buttons[i]->x + buttons[i]->width)){
+					if ((touch.py >= buttons[i]->y) && (touch.py <= buttons[i]->y + buttons[i]->height)){
+						buttons[i]->frame = (buttons[i]->frame==0) ? 1 : 0;
+					}
+				}
+			}
+		}
+		lastFramePenDown = penDown;	
+		//iprintf("\x1b[6;5HTouch x = %04X, %04X\n", touch.rawx, touch.px);
+
+		// Rendering
 		for(int i=0; i < spriteCount; i++){
 			drawSprite(&sprites[i]);
 		}
@@ -88,7 +108,6 @@ int main(void) {
 		int pressed = keysDown();
 		if(pressed & KEY_UP){
 			bgScroll(bg3, 0,1);
-			sprites[0].frame = 1;
 		}
 		bgUpdate();
 	}
